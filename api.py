25df62d38bd89cd1a114c64b568bfbcef4720923
BroSmithBoris <47716016+BroSmithBoris@users.noreply.github.com>
2020-07-05 20:46:16 +0500

    2

--- api.py
+++ api.py
@@ -1,34 +1,34 @@
 import requests
+import re
 
 count = 0
 url = 'https://api.hh.ru/vacancies/'
-need_skills = ['SQL', 'Oracle']
+# need_skills = ['SQL', 'Oracle']
+
 
 par = {'text': '("Проблемно-ориентированный" OR "Проблемно ориентированный") '
                'OR (Администратор OR Administrator OR Аналитик or Analyst) '
                'AND (производительности OR "баз данных" OR бд OR "хранилища данных" OR Database OR DB) '
                'OR (АБД OR DBA OR СУБД OR "Система управления базами данных" OR "Database Management System" OR DBMS OR DMS)',
-       'premium': 'false', 'area': 1261, 'per_page': '100', 'page': 1}
-
+       'premium': 'false', 'area': 1261, 'per_page': '100', 'page': 2}
+print('Порядок\tID Вакансии\tОбъявление\tГород\tКомпания\tКлючевые навыки')
 for i in requests.get(url, params=par).json()['items']:
-    flag = False
     vacancy_string = ''
-    key_skills_string = ''
+    key_skills_string = '"'
+    snippet_string = '"'
+
     vac_id = i['id']
     vac_name = i['name']
-    vacancy = requests.get('https://api.hh.ru/vacancies/' + vac_id).json()
 
+    vacancy = requests.get('https://api.hh.ru/vacancies/' + vac_id).json()
     key_skills = vacancy['key_skills']
     for e in key_skills:
         skill = e['name']
         if skill is not None:
-            key_skills_string += ' ' + skill + ','
-            if need_skills.count(skill):
-                flag = True
-    if flag:
-        area = vacancy['area']
-        employer = vacancy['employer']
-        count += 1
-        vacancy_string += str(count) + ' ' + vac_id + ' ' + vac_name + '; ' + area['name'] + '; ' + employer['name']
-        vacancy_string += ';'
-        print(vacancy_string + key_skills_string)
+            key_skills_string += skill + '\\&&\\'
+    key_skills_string += '"'
+    area = vacancy['area']
+    employer = vacancy['employer']
+    count += 1
+    vacancy_string += str(count) + '\t' + vac_id + '\t' + vac_name + '\t' + area['name'] + '\t' + employer['name'] + '\t'
+    print(vacancy_string + key_skills_string)
